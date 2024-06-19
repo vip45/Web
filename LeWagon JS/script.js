952376274ff199b0f5b4b6c509ab0a4bbaa7bf84
Vipin Yadav <vipinvyadav9@gmail.com>
2024-06-19 21:36:27 +0530

    done some practices

---
+++ LeWagon JS/script.js
@@ -0,0 +1,13 @@
+const musician = {
+    fname:  "vipin",
+    lname: "yadav"
+};
+
+// alert([musician.fname,musician.lname])
+
+const firstname = "Hello";
+const secondname = "World";
+
+const both = firstname + secondname;
+console.log(both);
+alert(both);
